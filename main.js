4d0b07fe745dfdae0fecbc8809377a00397b4b0f
Florian Borie <florian.borie@ynov.com>
2021-10-08 15:30:06 +0200

    update one yahtzee

--- main.js
+++ main.js
@@ -1,7 +1,31 @@
-function firstLancerOne(result){
-    if(result == 1){
-        return 1
-    }
-    return 0
+Array.prototype.count = function(predicate = obj => true) {
+    return this.filter(predicate).length;
+  };
+
+function firstLancerOne(rolls){
+    // if(rolls == 1){
+    //     return 1
+    // }
+    // return 0
+    return calculateNumber(rolls, 1)
 }
+
+
+function calculateNumber(rolls, value){
+    console.log(rolls)
+    let test = rolls.count(dice => dice == value);
+    return value * test;
+}
+
+
 module.exports = firstLancerOne;
+
+// function firstLancerTwo(rolls){
+//     if(rolls == 2){
+//         return 2
+//     }
+//     return 0
+//     return calculateNumber(rolls, 2)
+// }
+
+// module.exports = firstLancerTwo;
--- main.test.js
+++ main.test.js
@@ -1,24 +1,51 @@
+const firstLancerTwo = require("./main");
 const firstLancerOne = require("./main")
 
-const data = [
+const dataOne = [
     {
-        sides: [1, 2, 3, 4, 5],
+        rolls: [1, 2, 3, 4, 5],
         result : 1,
     },
     {
-        sides: [2, 2, 3, 4, 5],
+        rolls: [2, 2, 3, 4, 5],
         result: 0,
     },
     {
-        sides: [1, 2, 1, 4, 5],
+        rolls: [1, 2, 1, 4, 5],
         result: 2,
-    },
+    }
   ];
 
-  describe.each(data)(`nombre de un `, (one) => {
-    it(`le résultat ${one.sides} correspond à ${one.result}`, () => {
-      const result = firstLancerOne(...one.sides);
+  describe.each(dataOne)(`nombre de un `, (one) => {
+    it(`le résultat ${one.rolls} correspond à ${one.result}`, () => {
+      const result = firstLancerOne(one.rolls);
+      // const result = firstLancerOne([1,2,3,4,5]);
 
       expect(result).toBe(one.result);
     });
   });
+
+
+  // const dataTwo = [
+  //   {
+  //       rolls: [1, 2, 3, 4, 5],
+  //       result : 2,
+  //   },
+  //   {
+  //       rolls: [1, 1, 3, 4, 5],
+  //       result: 0,
+  //   },
+  //   {
+  //       rolls: [1, 2, 1, 4, 5],
+  //       result: 2,
+  //   },
+  // ];
+
+  // describe.each(dataTwo)(`nombre de un `, (two) => {
+  //   it(`le résultat ${two.rolls} correspond à ${two.result}`, () => {
+  //     const result = firstLancerTwo(...two.rolls);
+  //     // const result = firstLancertwo([1,2,3,4,5]);
+
+  //     expect(result).toBe(two.result);
+  //   });
+  // });
