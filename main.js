475a2876c3891f049d6e45a0d7455a05d46cf87b
Florian Borie <florian.borie@ynov.com>
2021-10-08 17:48:46 +0200

    add begin method 1 à 4

--- main.js
+++ main.js
@@ -1,19 +1,45 @@
-Array.prototype.count = function(predicate = obj => true) {
-    return this.filter(predicate).length;
-};
+module.exports ={
+    Count: Array.prototype.count = function(predicate = obj => true) {
+        return this.filter(predicate).length;
+    },
+    
+    One: function firstLancerOne(rolls){
+        return this.Final(rolls, 1)
+    },
+    
+    Two: function firstLancerTwo(rolls) {
+        return this.Final(rolls, 2)
+    },
 
-function firstLancerOne(rolls){
-    return calculateNumber(rolls, 1)
+    Three: function firstLancerThree(rolls) {
+        return this.Final(rolls, 3)
+    },
+
+    Four: function firstLancerFour(rolls) {
+        return this.Final(rolls, 4)
+    },
+    
+    Final: function calculateNumber(rolls, value){
+        let test = rolls.count(dice => dice == value);
+        return value * test;
+    }
 }
+// Array.prototype.count = function(predicate = obj => true) {
+//     return this.filter(predicate).length;
+// };
 
-function firstLancerTwo(rolls) {
-    return calculateNumber(rolls, 2)
-}
+// function firstLancerOne(rolls){
+//     return calculateNumber(rolls, 1)
+// }
 
-function calculateNumber(rolls, value){
-    let test = rolls.count(dice => dice == value);
-    return value * test;
-}
+// function firstLancerTwo(rolls) {
+//     return calculateNumber(rolls, 2)
+// }
 
-module.exports = firstLancerOne, firstLancerTwo
+// function calculateNumber(rolls, value){
+//     let test = rolls.count(dice => dice == value);
+//     return value * test;
+// }
+
+// module.exports = firstLancerOne, firstLancerTwo
 // module.exports = firstLancerTwo;
--- main.test.js
+++ main.test.js
@@ -1,6 +1,7 @@
 const firstLancerOne = require("./main");
-
 const firstLancerTwo = require("./main");
+const firstLancerThree = require("./main");
+const firstLancerFour = require("./main");
 
 const dataOne = [
     {
@@ -19,7 +20,7 @@ const dataOne = [
 
   describe.each(dataOne)(`nombre de un `, (one) => {
     it(`le résultat ${one.rolls} correspond à ${one.result}`, () => {
-      const result = firstLancerOne.testOne(one.rolls);
+      const result = firstLancerOne.One(one.rolls);
       // const result = firstLancerOne([1,2,3,4,5]);
 
       expect(result).toBe(one.result);
@@ -44,9 +45,58 @@ const dataOne = [
 
   describe.each(dataTwo)(`nombre de deux `, (two) => {
     it(`le résultat ${two.rolls} correspond à ${two.result}`, () => {
-      const result = firstLancerTwo.testTwo(two.rolls);
+      const result = firstLancerTwo.Two(two.rolls);
       // const result = firstLancertwo([1,2,3,4,5]);
 
       expect(result).toBe(two.result);
+    });
+  });
+
+  const dataThree = [
+    {
+        rolls: [1, 2, 3, 4, 5],
+        result : 3,
+    },
+    {
+        rolls: [1, 1, 2, 4, 5],
+        result: 0,
+    },
+    {
+        rolls: [1, 3, 3, 2, 4],
+        result: 6,
+    }
+  ];
+
+  describe.each(dataThree)(`nombre de trois `, (three) => {
+    it(`le résultat ${three.rolls} correspond à ${three.result}`, () => {
+      const result = firstLancerThree.Three(three.rolls);
+      // const result = firstLancertwo([1,2,3,4,5]);
+
+      expect(result).toBe(three.result);
+    });
+  });
+
+
+  const dataFour = [
+    {
+        rolls: [1, 2, 3, 4, 5],
+        result : 4,
+    },
+    {
+        rolls: [1, 1, 2, 6, 5],
+        result: 0,
+    },
+    {
+        rolls: [1, 3, 4, 2, 4],
+        result: 8,
+    }
+  ];
+
+  describe.each(dataFour)(`nombre de quatre `, (four) => {
+    it(`le résultat ${four.rolls} correspond à ${four.result}`, () => {
+      const result = firstLancerFour.Four(four.rolls);
+      // const result = firstLancertwo([1,2,3,4,5]);
+
+      expect(result).toBe(four.result);
     });
   });
